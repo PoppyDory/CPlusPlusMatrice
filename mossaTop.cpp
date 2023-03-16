c730bf6e87a871c73a2efcc5db533338735075b9
PoppyDory <lanza.rosita@gmail.com>
2023-03-16 14:34:57 +0100

    mossa vincente

--- mossaTop.cpp
+++ mossaTop.cpp
@@ -32,14 +32,15 @@ int main()
     stampa(tab);
 
     /*
-        X _ X       O _ _       O _ X       _ X _       X _ _       _ X X
-        _ O _       _ X _       _ X _       _ _ O       _ _ _       _ O _        TENTATIVI  
-        _ _ _       X _ _       _ _ _       _ X _       X _ O       _ _ _  
+        X _ X       O _ _       O _ X       _ X _       X _ _       _ X X       O O _
+        _ O _       _ X _       _ X _       _ _ O       _ _ _       _ O _       X _ _        TENTATIVI  
+        _ _ _       X _ _       _ _ _       _ X _       X _ O       _ _ _       X _ _  
     */
 
-    tab[1] = 'X';
-    tab[2] = 'X';
-    tab[6] = 'O';
+    tab[0] = 'O';
+    tab[1] = 'O';
+    tab[3] = 'X';
+    tab[6] = 'X';
 
     stampa(tab);
 
@@ -98,5 +99,50 @@ int main()
         }
     }
     
-    cout << "risultato ottenuto: " << risultato << " | risultato atteso: 0" << endl;
+    // ora controllo se il computer può vincere
+    // se ci sono due O metto il terzo
+    
+    for (int i = 0; i < 8; i++)
+    {
+        char a = tab[combinazioni[i][0]];
+        char b = tab[combinazioni[i][1]];
+        char c = tab[combinazioni[i][2]];
+
+        bool condizione = a == b && c == vuoto && a == 'O';
+
+        if (condizione)
+        {
+            risultato = combinazioni[i][2];
+        }
+    }
+
+    for (int i = 0; i < 8; i++)
+    {
+        char a = tab[combinazioni[i][0]];
+        char b = tab[combinazioni[i][1]];
+        char c = tab[combinazioni[i][2]];
+
+        bool condizione = c == b && a == vuoto && c == 'O';
+
+        if (condizione)
+        {
+            risultato = combinazioni[i][0];
+        }
+    }
+    
+    for (int i = 0; i < 8; i++) // risultato atteso: 1
+    {
+        char a = tab[combinazioni[i][0]];
+        char b = tab[combinazioni[i][1]];
+        char c = tab[combinazioni[i][2]];
+
+        bool condizione = a == c && b== vuoto && c == 'O';
+
+        if (condizione)
+        {
+            risultato = combinazioni[i][1];
+        }
+    } 
+    
+    cout << "risultato ottenuto: " << risultato << " | risultato atteso: 2" << endl;
 }
